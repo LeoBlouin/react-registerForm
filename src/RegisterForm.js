9ab24b1db82d18f872cd2d4ab6f87192c82622cd
Léo Blouin <leoblouin@outlook.fr>
2021-03-11 09:53:37 +0100

    Interface

---
+++ src/RegisterForm.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+
+class RegisterForm extends Component {
+
+  state = {
+    validateChars: false,
+    validateMin: false,
+    validateMaj: false,
+    validateNumber: false,
+    password: '',
+    email: '',
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    this.checkPassword(e.target.value);
+  }
+
+  checkPassword = (password) => {
+    this.setState({
+      validateNumber: /\d/.test(password)
+    });
+  }
+
+  render() {
+    return (
+      <div className="grid grid-cols-12 grid-rows-1 my-20">
+        <div
+          className="col-span-12 sm:col-span-8 sm:col-start-3 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5 bg-gray-50 p-3 sm:px-6 sm:py-8 rounded-lg">
+          <h3 className="font-bold text-center text-3xl mb-10">Inscription</h3>
+          <form>
+            <label className="block" htmlFor="email">Adresse email</label>
+            <input type="email" placeholder="Entrez une adresse email"
+                   name="email"
+                   value={this.state.email}
+                   onChange={this.onChange}
+                   className="border mt-2 w-full py-2 px-4 rounded-md duration-300 outline-none focus:ring"/>
+
+            <label className="block mt-5" htmlFor="password">Mot de passe</label>
+            <input type="password" placeholder="Entrez un mot de passe"
+                   className="border mt-2 w-full py-2 px-4 rounded-md duration-300 outline-none focus:ring"
+                   name="password"
+                   value={this.state.password}
+                   onChange={this.onChange}/>
+
+            <div className="mt-5 font-semibold">
+              <p>
+                {this.state.validateChars ? (<i className="fas fa-check text-green-400 mr-4"/>) : (
+                  <i className="fas fa-times text-red-500 mr-5 ml-0.5"/>)}
+                <span>8 caractères minimum</span>
+              </p>
+
+              <p>
+                {this.state.validateMin ? (<i className="fas fa-check text-green-400 mr-4"/>) : (
+                  <i className="fas fa-times text-red-500 mr-5 ml-0.5"/>)}
+                <span>1 minuscule minimum</span>
+              </p>
+
+              <p>
+                {this.state.validateMaj ? (<i className="fas fa-check text-green-400 mr-4"/>) : (
+                  <i className="fas fa-times text-red-500 mr-5 ml-0.5"/>)}
+                <span>1 majuscule minimum</span>
+              </p>
+
+              <p>
+                {this.state.validateNumber ? (<i className="fas fa-check text-green-400 mr-4"/>) : (
+                  <i className="fas fa-times text-red-500 mr-5 ml-0.5"/>)}
+                <span>1 chiffre minimum</span>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default RegisterForm;
